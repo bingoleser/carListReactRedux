71449f8439d6a61ff8a662d57c72da0338e869ed
eser bingol <bingoleser21@gmail.com>
2023-05-29 13:52:46 +0300

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+          Learn React
+     <h1>car list</h1>
+     <button>click here</button>
+    </div>
+  );
+}
+
+export default App;
